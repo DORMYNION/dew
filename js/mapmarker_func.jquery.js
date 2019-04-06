54bd944eda1ae2dc664d08ff3ea03bab08adb47a
dormynion <dlorunfemi@gmail.com>
2019-04-06 14:34:57 +0100

    txt

--- js/mapmarker_func.jquery.js
+++ js/mapmarker_func.jquery.js
@@ -1,14 +1,14 @@
 		$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 		//set up markers 
 		var myMarkers = {"markers": [
-				{"latitude": "51.511732", "longitude":"-0.123270", "icon": "img/map-marker.png"}
+				{"latitude": "7.368117", "longitude":"3.837157", "icon": "img/map-marker.png"}
 			]
 		};
 		
 		//set up map options
 		$("#map_contact").mapmarker({
 			zoom	: 14,
-			center	: 'Covent Garden London',
+			center	: 'DEW',
 			markers	: myMarkers
 		});
 
